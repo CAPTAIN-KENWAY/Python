f0ff3dd290402544de764a403282752554b736cd
CAPTAIN-KENWAY <karangpt69@gmail.com>
2023-01-06 16:28:16 +0530

    Completed - 06/01/2023

--- Blog_Final/forms.py
+++ Blog_Final/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import InputRequired, URL, Email
+from wtforms import StringField, SubmitField, PasswordField, EmailField, TextAreaField
+from wtforms.validators import InputRequired, URL, Email, Length, Regexp
 from flask_ckeditor import CKEditorField
 
 
-##WTForm
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[InputRequired()])
     subtitle = StringField("Subtitle", validators=[InputRequired()])
@@ -12,13 +12,33 @@ class CreatePostForm(FlaskForm):
     body = CKEditorField("Blog Content", validators=[InputRequired()])
     submit = SubmitField("Submit Post")
 
+
 class RegisterForm(FlaskForm):
     email = EmailField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
     name = StringField('Name', validators=[InputRequired()])
     submit = SubmitField("Sign Me Up!")
 
+
 class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField("Let Me In!")
+
+
+class CommentForm(FlaskForm):
+    comment = CKEditorField("Comment", validators=[InputRequired()])
+    submit = SubmitField("Submit Comment")
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    email = EmailField('Email Address', validators=[InputRequired(), Email()])
+    phone = StringField(
+        'Phone Number',
+        validators=[InputRequired(),
+                    Length(min=10, max=10),
+                    Regexp(regex='^[+-]?[0-9]+$')]
+        )
+    message = TextAreaField('Message', validators=[InputRequired()])
+    submit = SubmitField("SEND")
--- Blog_Final/main.py
+++ Blog_Final/main.py
@@ -6,9 +6,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm, RegisterForm, LoginForm
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm, ContactForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import smtplib
+
+
+EMAIL = "pythontesting810@gmail.com"
+PASSWORD = "jflzmvkrbbuhavxz"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -23,6 +28,10 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app=app)
 
+gravatar = Gravatar(
+    app, rating='g', size=20, default='retro', force_default=False, force_lower=False, use_ssl=False,
+    base_url=None)
+
 
 def admins_only(func):
     wraps(func)
@@ -34,8 +43,32 @@ def admins_only(func):
             return abort(403)
     return decorated_function
 
+def send_mail(name, email, phone, message):
+    msg = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs="guptakaran094@gmail.com",
+            msg=msg.encode('utf-8')
+        )
+
 
 # CONFIGURE TABLES
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    #author = db.relationship('User', back_populates='posts')
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='post')
 
 
 class User(db.Model, UserMixin):
@@ -44,19 +77,16 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
-    posts = db.relationship('BlogPost', backref='users')
+    posts = db.relationship('BlogPost', backref='author')
+    comments = db.relationship('Comment', backref='user')
 
 
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
+class Comment(db.Model):
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(250), nullable=False)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.name'))
+    comment = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
 
 
 # db.create_all()
@@ -118,10 +148,23 @@ def logout():
         return redirect(url_for('home'))
 
 
-@app.route("/post/<int:post_id>")
+@app.route("/post/<int:post_id>", methods=['POST', 'GET'])
 def show_post(post_id):
+    form = CommentForm()
     requested_post = BlogPost.query.get(post_id)
-    return render_template("post.html", post=requested_post)
+    comments = db.session.query(Comment).filter_by(post_id=requested_post.id)
+    if form.validate_on_submit():
+        user = current_user.id
+        post = requested_post.id
+        new_comment = Comment(
+            comment=form.comment.data,
+            user_id=user,
+            post_id=post
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=requested_post.id))
+    return render_template("post.html", post=requested_post, form=form, comments=comments, avatar=gravatar)
 
 
 @app.route("/about")
@@ -129,9 +172,20 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['POST', 'GET'])
 def contact():
-    return render_template("contact.html")
+    form = ContactForm()
+    if form.validate_on_submit():
+        send_mail(form.name.data, form.email.data, form.phone.data, form.message.data) 
+        form = ContactForm(formdata=None)
+        return render_template("contact.html", msg_sent=True, form=form)
+
+    # if request.method == 'GET':
+    #     return render_template("contact.html", msg_sent=False)
+    # else:
+    #     data = request.form
+    #     send_mail(data['name'], data['email'], data["phone"], data["message"])        
+    return render_template("contact.html", msg_sent=False, form=form)
 
 
 @app.route("/new-post", endpoint='add', methods=['GET', 'POST'])
@@ -139,12 +193,13 @@ def contact():
 def add():
     form = CreatePostForm()
     if form.validate_on_submit():
+        author = current_user.id
         new_post = BlogPost(
             title=form.title.data,
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            author=current_user.name,
+            author_id=author,
             date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_post)
@@ -161,14 +216,14 @@ def edit(post_id):
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
+        # author=post.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
+        # post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
@@ -185,6 +240,6 @@ def delete(post_id):
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # with app.app_context():
+    #     db.create_all()
+    app.run(host='0.0.0.0', port=5000)
